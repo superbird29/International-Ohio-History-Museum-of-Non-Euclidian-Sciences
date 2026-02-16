9e0324c15e5d5c12207c23c648d8674db853790a
superbird29 <31712512+superbird29@users.noreply.github.com>
2026-02-16 14:10:59 -0600

    push for work on Orders

---
+++ Assets/Scripts/SpecialOrder.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialOrder : MonoBehaviour
+{
+    /// <summary>
+    /// Called when an npc wants to trigger an order
+    /// </summary>
+    public void Order()
+    {
+        //GenerateGiftOrder()
+    }
+}
