20956ac6be07c525dca19b77105b13f2f0178959
PandaDrummer117 <coda117@live.com>
2026-02-16 00:10:44 -0600

    Walls and Decorations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
+
 public class PlayerMovement : MonoBehaviour
 {
     public float movementSpeed;
     public Rigidbody2D rb2d;
     private Vector2 moveInput;
+    private Animator animator;
+    public float movementThreshold = 0.01f;
+
+    private void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
 
     private void Update()
     {
@@ -15,10 +23,26 @@ public class PlayerMovement : MonoBehaviour
         moveInput.y = Input.GetAxisRaw("Vertical");
 
         moveInput.Normalize();
-        rb2d.velocity = moveInput * movementSpeed;
     }
 
 
+    void FixedUpdate()
+    {
+        rb2d.velocity = moveInput * movementSpeed;
 
+        if (rb2d.velocity.magnitude > movementThreshold)
+        {
+            animator.SetBool("isWalking", true);
+            animator.SetFloat("InputX", moveInput.x);
+            animator.SetFloat("InputY", moveInput.y);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetFloat("LastInputX", animator.GetFloat("InputX"));
+            animator.SetFloat("LastInputY", animator.GetFloat("InputY"));
+        }
+
+    }
 
 }
