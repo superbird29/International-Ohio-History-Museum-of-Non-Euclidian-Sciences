e453065c8fd65f6889fa377070837b5c6e7ad247
superbird29 <31712512+superbird29@users.noreply.github.com>
2026-02-18 02:25:19 -0600

    push up for work done

--- Assets/Scripts/Scriptable Objects/Gifts/MerfolkSlime.cs
+++ Assets/Scripts/Scriptable Objects/Gifts/MerfolkSlime.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[CreateAssetMenu(fileName = "GiftOrders", menuName = "ScriptableObjects/New JacklopeHorns", order = 3)]
 public class MerfolkSlime : GiftOrder
 {
     public override void DoAction()
